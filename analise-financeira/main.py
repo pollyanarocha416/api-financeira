b9503012dbe2e53f5be046e36d40e72d1904e34c
pollyanarocha416 <pollyanarocha416@gmail.com>
2025-02-13 09:12:55 -0400

    VERIFICACAO SE ACAO É VALIDA, CONF SEPARADA DO RESTO DO CODIGO

--- analise-financeira/main.py
+++ analise-financeira/main.py
@@ -7,12 +7,12 @@
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 import requests
+from main_conf import API_URL, API_TOKEN
+
+
 
 app = FastAPI()
 
-
-API_TOKEN = 'jrfFdSF8RZ1scxbTk8baH7'
-API_URL = f'https://brapi.dev/api/quote/'
 
 
 @app.get('/acao')
@@ -21,12 +21,14 @@ async def get_acoes(acao: str):
     try:
         
         response = requests.get(API_URL+acao, headers=headers)
-        
-        nome_acao = response.json().get('results')[0].get('longName')
-        result = response.json().get('results')[0]
-        if response.status_code == 200:
-            return {"status": "success", f"ação {nome_acao}": result}
+        if response:
+            nome_acao = response.json().get('results')[0].get('longName')
+            result = response.json().get('results')[0]
+            if response.status_code == 200:
+                return {"status": "success", f"ação {nome_acao}": result}
+            else:
+                raise HTTPException(status_code=response.status_code, detail=response.text)
         else:
-            raise HTTPException(status_code=response.status_code, detail=response.text)
+            raise HTTPException(status_code=404, detail="Ação não encontrada")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
---
+++ analise-financeira/main_conf.py
@@ -0,0 +1,2 @@
+API_TOKEN = "jrfFdSF8RZ1scxbTk8baH7"
+API_URL = "https://brapi.dev/api/quote/"
